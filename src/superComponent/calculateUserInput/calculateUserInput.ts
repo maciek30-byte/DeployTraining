124cbe34c73f35a80914135b05fcfdcfbf3e5f42
maciek <maciek29.opozda@gmail.com>
2025-02-13 11:45:55 +0100

    PR-12 Enabled protection rules in branch this commit should not be merged

--- src/superComponent/calculateUserInput/calculateUserInput.ts
+++ src/superComponent/calculateUserInput/calculateUserInput.ts
@@ -5,3 +5,8 @@ export const calculateUserInput = (arrayToSum: number[])=>{
 export const displayLastExpenseValue = (arrayToCheck: number[])=>{
     return arrayToCheck[arrayToCheck.length-1]
 }
+
+export const removeLastElementFromArray = (array:number[])=>{
+
+    return array.slice(0, array.length-1)
+}
