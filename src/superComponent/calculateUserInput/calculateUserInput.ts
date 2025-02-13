b093acde9fa83cc07d3236f7ab81dbcd679ba93f
maciek <maciek29.opozda@gmail.com>
2025-02-13 09:17:23 +0100

    PR-11 Add automatic process trigger and simple change to test it

--- src/superComponent/calculateUserInput/calculateUserInput.ts
+++ src/superComponent/calculateUserInput/calculateUserInput.ts
@@ -1,3 +1,7 @@
 export const calculateUserInput = (arrayToSum: number[])=>{
     return arrayToSum.reduce((acc, curr)=> acc+=curr,0)
 }
+
+export const displayLastExpenseValue = (arrayToCheck: number[])=>{
+    return arrayToCheck[arrayToCheck.length-1]
+}
