242889a8f29e5b00a6bcdae8f6b02396a277626b
maciek <maciek29.opozda@gmail.com>
2025-02-13 05:52:41 +0100

    PR-5 Add vercel deployment job

---
+++ src/superComponent/superComponent.tsx
@@ -0,0 +1,10 @@
+export const SuperComponent = ()=>{
+    return(
+        <>
+        <h1>I am super component</h1>
+            <div>
+                <input name={'name'} />
+            </div>
+        </>
+    )
+}
